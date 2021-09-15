26617faff9670a5bdabf76afb77ebc7acbfc65b3
ext.lipeng12 <ext.lipeng12@jd.com>
2021-09-15 16:37:57 +0800

    规范一级目录

---
+++ src/index.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: your name
+ * @Date: 2021-09-15 16:35:43
+ * @LastEditTime: 2021-09-15 16:36:58
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /fast-cache-ddza/src/index.js
+ */
+
+alert('fast-cache')
